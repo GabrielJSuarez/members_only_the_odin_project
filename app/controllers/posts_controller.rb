173f7e4aa80881cb0672032f1d3cbe2c1f1d64d6
Gabriel Ginn Suarez <ginnlaiho@gmail.com>
2020-11-26 14:33:07 -0600

    bugfix: changed index to show post creation method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,8 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order('order_by DESC')
+    @post = Post.new
   end
 
   # GET /posts/1
